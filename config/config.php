ec988d62ce96f38167488d7a7a21eb2ff71fe26b
brajinderism <brajinder.singh.1469@gmail.com>
2021-07-27 14:56:05 +0530

    Fixing bugs

--- config/config.php
+++ config/config.php
@@ -4,8 +4,8 @@
  * You can place your custom package configuration in here.
  */
 return [
-	"KEY-ID" => env("AWS_KEY_ID", NULL),
-	"AWS-PRIVATE-KEY" => env("AWS_PRIVATE_KEY_FILENAME", NULL),
+	"KEY_ID" => env("AWS_KEY_ID", NULL),
+	"AWS_PRIVATE_KEY" => env("AWS_PRIVATE_KEY_FILENAME", NULL),
 	"CLIENT_IP" => env("AWS_CLIENT_IP", NULL),
 	"AWS_URL" => env("AWS_URL", NULL),
 ];
